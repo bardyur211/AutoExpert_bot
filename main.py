cb68dc125389690675c34c5b5300233befa1748a
baryur21 <zidkovgrigorij297@gmail.com>
2024-09-18 22:51:27 +0300

    v1.0.1

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import asyncio
 import logging
 import sys
-import pathlib
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import Command, CommandObject, CommandStart
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram.types import (ContentType, Message, CallbackQuery, KeyboardButton,
-                           InlineKeyboardButton, ReplyKeyboardMarkup)
+from aiogram.types import ContentType, Message
+from aiogram.fsm.storage.memory import MemoryStorage
 import keyboard
 from Config import TOKEN
 
@@ -17,13 +14,17 @@ logging.basicConfig(level=logging.INFO)
 # BOt token and dispatcher
 BOT = Bot(token=TOKEN)
 dp = Dispatcher()
+storage = MemoryStorage()
 
 
 @dp.message(F.content_type == ContentType.NEW_CHAT_MEMBERS)
 async def new_member_handler(message: Message):
     new_member = message.new_chat_members[0]
-    await message.answer(f"""Привет, {new_member.full_name}! Мы рады что ты с нами!
-    Перед тем как задавать вопросы, лучше ознакомься с правилами и основными разделами группы""",
+    message_answer = f"""Привет, [{new_member.full_name}](tg://user?id={new_member.id})! Мы рады что ты с нами!
+Перед тем как задавать вопросы, лучше ознакомься с правилами и основными разделами группы"""
+    message_answer = message_answer.replace("!", "\\!")
+    await message.answer(message_answer,
+                         parse_mode='MarkdownV2',
                          reply_markup=keyboard.main_kb)
 
 
